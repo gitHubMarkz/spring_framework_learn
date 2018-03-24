e214b8b07d369380684d1d7d31fa47da9f455c56
markz <markz@163.com>
2018-03-24 15:59:39 +0800

    init

---
+++ spring-webmvc/src/main/java/org/springframework/web/servlet/tags/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Spring's JSP standard tag library for JSP 2.0+.
+ * Supports JSP view implementations within Spring's web MVC framework.
+ * See {@code spring.tld} for descriptions of the various tags.
+ */
+@NonNullApi
+@NonNullFields
+package org.springframework.web.servlet.tags;
+
+import org.springframework.lang.NonNullApi;
+import org.springframework.lang.NonNullFields;
